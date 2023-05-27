3c8714679683135b24a599e7c19bd34650d7c74b
ratul <ratulsikder104@gmail.com>
2023-05-27 09:10:40 +0600

    Login Page Desing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  app: {
+    head: {
+      link:[{
+        href: 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap',
+        rel: 'stylesheet'
+      }]
+    }
+  },
+  css: [
+    '@/assets/styles/main.scss'
+  ],
   plugins: [
     // Other plugins...
     { src: "./plugins/strophe-config.ts", mode: "client" },
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,25 +1,23 @@
 <script setup>
 
-
-console.log(Strophe);
-
-
-// function onConnect(status) {
-//     if (status === Strophe.Status.CONNECTING) {
-//         console.log('Connecting...');
-//     } else if (status === Strophe.Status.CONNFAIL) {
-//         console.log('Connection failed.');
-//     } else if (status === Strophe.Status.DISCONNECTING) {
-//         console.log('Disconnecting...');
-//     } else if (status === Strophe.Status.DISCONNECTED) {
-//         console.log('Disconnected.');
-//     } else if (status === Strophe.Status.CONNECTED) {
-//         console.log('Connected.');
-//         // conn.addHandler(onChatMessage, null, "message", "chat");
-//     }
-// }
 </script>
 
 <template>
-    Login
+    <div id="login">
+        <h1>Login</h1>
+
+        <div class="content">
+            <div class="input-group">
+                <label for="">Email</label>
+                <input type="text" name="email" v-model="email" :autocomplete="false">
+            </div>
+
+            <div class="input-group">
+                <label for="">Password</label>
+                <input type="password" name="password" v-model="password" :autocomplete="false">
+            </div>
+
+            <button class="btn btn-primary">Login</button>
+        </div>
+    </div>
 </template>
